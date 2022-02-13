f9060e04e91ab95b03e10324fb8449da3ba89f0c
kento-furusho <84021293+kento-furusho@users.noreply.github.com>
2022-02-13 18:52:45 +0900

    initialized application.rb

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,17 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # railsアプリのタイムゾーン設定
+    config.time_zone = ENV["TZ"]
+
+    # データベース読み書き時のタイムゾーン設定
+    config.active_record.default_timezone = :utc
+
+    # i18nでのデフォルトロケールファイルの設定
+    config.i18n.default_locale = :ja
+
+    
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
