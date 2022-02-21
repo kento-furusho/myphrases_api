520e94f4a09735e4081a9c876c64742837f1837d
kento-furusho <84021293+kento-furusho@users.noreply.github.com>
2022-02-21 16:18:14 +0900

    detail fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
       namespace :v1, defaults: { format: :json } do
         mount_devise_token_auth_for "User", at: "auth", controllers: {
-          registrarions: 'api/v1/registrations',
+          registrarions: '/api/v1/registrations',
           sessions: 'devise_token_auth/sessions'
         }
         resources :users, only:[:index]
