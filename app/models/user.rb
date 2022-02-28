b44feaea53d36cd0cb207b6474bcefdec342338d
kento-furusho <84021293+kento-furusho@users.noreply.github.com>
2022-02-28 15:09:35 +0900

    add usdr validation and ja.yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
   include DeviseTokenAuth::Concerns::User
+  # 以下独自バリデーション
+  validates :name, presence: true,
+                   length: {  maximum: 30, allow_blank: true }
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+  validates :password, presence: true,
+                       length: { minimum: 8 },
+                       format: {
+                         with: VALID_PASSWORD_REGEX,
+                         message: :invalid_password
+                       },
+                       allow_nil: true
 end
